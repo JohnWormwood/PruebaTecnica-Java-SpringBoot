e83fa3d331eddab9e1772ffbd9963a211c578590
JohnWormwood <127157172+JohnWormwood@users.noreply.github.com>
2025-04-25 22:44:35 +0200

    First Commit

---
+++ src/main/java/com/prueba/tecnica/TecnicaApplication.java
@@ -0,0 +1,16 @@
+package com.prueba.tecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnicaApplication.class, args);
+
+
+		System.out.println("hola mundo");
+	}
+
+}
