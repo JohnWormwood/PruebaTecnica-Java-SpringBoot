c3c08a362527c9061e21f9fe7c7ddcb3d8a1e2cd
JohnWormwood <127157172+JohnWormwood@users.noreply.github.com>
2025-04-26 14:37:57 +0200

    create TareaRepository interface

---
+++ src/main/java/com/prueba/tecnica/repository/TareaRepository.java
@@ -0,0 +1,9 @@
+package com.prueba.tecnica.repository;
+
+import com.prueba.tecnica.model.Tarea;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TareaRepository extends JpaRepository<Tarea, Long> {
+}
