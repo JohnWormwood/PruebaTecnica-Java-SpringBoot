7ea8c225d83d2340ebffa969d2f434433518fad7
JohnWormwood <127157172+JohnWormwood@users.noreply.github.com>
2025-04-27 11:13:47 +0200

    create TareaEntity

---
+++ src/main/java/com/prueba/tecnica/models/Tarea.java
@@ -0,0 +1,46 @@
+package com.prueba.tecnica.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import javax.annotation.processing.Generated;
+
+@Entity
+@Table(name = "tareas")
+public class Tarea {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String titulo;
+
+    private String descripcion;
+
+    private boolean completada = false;
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+}
