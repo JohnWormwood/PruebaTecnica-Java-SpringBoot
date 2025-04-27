6ab7cb1a7bf213dfa25a058bb60001eed6e131a9
JohnWormwood <127157172+JohnWormwood@users.noreply.github.com>
2025-04-27 12:33:35 +0200

    push test

--- src/main/java/com/prueba/tecnica/TecnicaApplication.java
+++ src/main/java/com/prueba/tecnica/TecnicaApplication.java
@@ -9,8 +9,6 @@ public class TecnicaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TecnicaApplication.class, args);
 
-
-		System.out.println("hola mundo");
 	}
 
 }
